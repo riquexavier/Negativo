fee018eda1c12695ea5f162ddf00027399b8a7d3
ETEC Adolpho Berezin <110746209+riquexavier@users.noreply.github.com>
2024-05-03 19:44:02 -0300

    PedrãoNegativado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+Console.Clear();
+int numero;
+
+Console.WriteLine("Digite um numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if( numero >= 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Seu número é positivo");
+Console.ResetColor();
+}
+
+else
+
+if( numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Seu número é negativo");
+Console.ResetColor();
+}
